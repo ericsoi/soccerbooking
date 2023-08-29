7137e7fb8746670a271e923c43f673515f08617c
ericsoi <ericksoi3709@gmail.com>
2023-08-29 22:39:57 +0300

    video css

--- components/VideoPlayer.jsx
+++ components/VideoPlayer.jsx
@@ -1,9 +1,14 @@
+import React from 'react';
+import styles from '../styles/VideoPlayer.module.css'; // Import your CSS module
+
 const VideoPlayer = () => {
-    return (
-      <video controls>
+  return (
+    <div className={styles.videoContainer}>
+      <video className={styles.video} controls>
         <source src="/video/VID-20230829-WA0005.mp4" type="video/mp4" />
       </video>
-    );
-  };
+    </div>
+  );
+};
 
-export default VideoPlayer
+export default VideoPlayer;
